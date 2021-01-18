342cb5fdd5233f988af09deb8528d3db0aeff026
maliayyildiz <you@example.com>
2021-01-19 00:24:35 +0100

    lastest update

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,2 +1,19 @@
-package pages;public class HomePage {
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public
+
+class HomePage {
+    public HomePage(){ PageFactory.initElements(Driver.getDriver(),this); }
+
+    @FindBy(id = "account-menu")
+    public WebElement userIcon;
+    @FindBy(id = "login-item")
+    public WebElement signIn;
+    @FindBy(xpath = "(//a[@class='dropdown-item'])[2]")
+    public WebElement register;
 }
