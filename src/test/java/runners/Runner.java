e0330587cda131d4e51e59de9d4b26de4c5c0e79
Zafer <zaferkok111@gmail.com>
2021-02-06 02:07:45 +0100

    US 24 ve US 29

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -15,7 +15,7 @@ import utilities.ReusableMethods;
                     "junit:target/xml-report/cucumber.xml" },
             features = "src/test/resources/features",
             glue = "stepdefinitions",
-            tags = "@demo",
+            tags = "@T28_164",
             dryRun = false,
             publish = false
     )
